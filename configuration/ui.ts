e091c760f0347b0e98ac2f190bec77477161a259
JinnyPark7 <jhp@business.unc.edu>
2025-02-27 18:29:48 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,6 +1,6 @@
-import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
+import { JINAI, Jinny Park } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Ask to {JINAI}`;
+export const CHAT_HEADER: string = `Ask tob JINAI`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Dr.D @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Clear`;
