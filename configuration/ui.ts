2763f3df892e9d0d056b12bc5a9b3c3f5f2dc982
JinnyPark7 <jhp@business.unc.edu>
2025-02-27 18:39:09 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Dr.D @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Clear`;
 export const PAGE_TITLE: string = `JINAI`;
-export const PAGE_DESCRIPTION: string = `Chat with {JINAI}, {Jinny Park}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with 'JINAI', 'Jinny Park''s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
