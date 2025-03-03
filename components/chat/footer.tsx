fb6c0499f63873630c273a9bb94e37f9e36d98fb
JinnyPark7 <jhp@business.unc.edu>
2025-03-02 20:23:13 -0500

    Update footer.tsx

--- components/chat/footer.tsx
+++ components/chat/footer.tsx
@@ -4,25 +4,18 @@ import Link from "next/link";
 export default function ChatFooter() {
   return (
     <div className="w-full text-xs flex pt-2 text-gray-500">
-      {/* Left Pane - Terms of Service */}
       <div className="flex-grow text-left">
+        {/* Left Pane */}
         <Link href="/terms" className="hover:underline">
           Terms of Service
         </Link>
       </div>
-
-      {/* Center Pane - Customized AI Message with Logo */}
       <div className="flex-grow text-center">
-        <div className="flex flex-col items-center">
-          {/* Custom logo image */}
-          <img src="/logo.png" alt="JINAI Logo" width="150" className="mb-2" />
-          {/* Customized AI message */}
-          <p>Customized AI by Jinny</p>
-        </div>
+        {/* Center Pane */}
+        {FOOTER_MESSAGE}
       </div>
-
-      {/* Right Pane - AI Provider Information */}
       <div className="flex-grow text-right">
+        {/* Right Pane */}
         {/* Do not remove or modify the following message. Removal or modification violates the license agreement. */}
         <a
           href="http://www.ringel.ai"
