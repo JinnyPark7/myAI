3b6e846bbfd6fdac21368f9a8d57272a080f662e
JinnyPark7 <jhp@business.unc.edu>
2025-03-02 20:09:21 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Jinny Park`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `A student studying AI`;
 
 export const AI_NAME: string = `JINAI`;
 export const AI_TONE: string = `[AI TONE]`;
